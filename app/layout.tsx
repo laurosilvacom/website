40430315a67f523136899a35d922a0d024414cc5
Lauro Silva <hey@laurosilva.com>
2025-01-30 10:35:03 -0800

    update content

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,15 +72,17 @@ const commitMono = localFont({
 export const metadata: Metadata = {
 	metadataBase: new URL(baseUrl),
 	title: {
-		default: 'Next.js Portfolio Starter',
-		template: '%s | Next.js Portfolio Starter'
+		default: 'Lauro Silva',
+		template: '%s | Lauro Silva'
 	},
-	description: 'This is my portfolio.',
+	description:
+		'Learn, build, and grow as a developer. Deep technical insights mixed with real experiences. Because great software comes from understanding both the code and the people who write it.',
 	openGraph: {
-		title: 'My Portfolio',
-		description: 'This is my portfolio.',
+		title: 'Lauro Silva',
+		description:
+			'Learn, build, and grow as a developer. Deep technical insights mixed with real experiences. Because great software comes from understanding both the code and the people who write it.',
 		url: baseUrl,
-		siteName: 'My Portfolio',
+		siteName: 'Lauro Silva',
 		locale: 'en_US',
 		type: 'website'
 	},
